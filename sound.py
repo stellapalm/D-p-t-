af042941ce2d7f2143c461eec8376b4aa7ecc127
stellapalm <71771543+stellapalm@users.noreply.github.com>
2021-10-01 14:51:38 +0200

    Add files via upload

---
+++ sound.py
@@ -0,0 +1,33 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices impot Motor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Button, Color, ImageFile, SoundFile
+from pybricks.tools import wait
+
+
+
+def blue_sound :
+    if color == Color.BLUE:
+            ev3.speaker.say('kung_fu')
+
+return color
+
+
+def green_sound :
+    if color == Color.GREEN
+            ev3.speaker.say('laughing_2')
+
+return color
+
+
+def yellow_sound :
+    if color == Color.YELLOW 
+            ev3.speaker.say('fanfare')
+
+return color
+
+
+def red_sound :
+    if color = Color.RED 
+            ev3.speaker.say('uh_oh')
+
+return color
